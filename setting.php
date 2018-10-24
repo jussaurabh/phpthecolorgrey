f299b892dd9f8de4ea15bb3b300229ae357bd6f1
jussaurabh <pawar.saurabh95@gmail.com>
2018-10-24 13:05:54 +0530

    added settings page

---
+++ setting.php
@@ -0,0 +1,30 @@
+<?php 
+
+// require "./includes/session.inc.php";
+
+
+include "./includes/header.inc.php";
+
+
+?>
+
+
+<main>
+
+
+   <div class="container">
+
+      
+
+   </div>
+
+
+</main>
+
+
+<?php 
+
+include "./includes/categories.inc.php";
+include "./includes/footer.inc.php";
+
+?>
